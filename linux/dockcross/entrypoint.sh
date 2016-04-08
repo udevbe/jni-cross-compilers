e8ba087799c48ff746c967e72239ee09244d79cb
Erik De Rijcke <derijcke.erik@gmail.com>
2016-04-08 23:42:27 +0200

    use sudo to perform commands

--- linux/dockcross/entrypoint.sh
+++ linux/dockcross/entrypoint.sh
@@ -20,7 +20,7 @@ if [[ -n $BUILDER_UID ]] && [[ -n $BUILDER_GID ]]; then
     useradd -o -g $BUILDER_GID -u $BUILDER_UID $BUILDER_USER 2> /dev/null
 
     # Run the command as the specified user/group.
-    exec chpst -u :$BUILDER_UID:$BUILDER_GID "$@"
+    exec sudo -u \#$BUILDER_UID -g \#$BUILDER_GID "$@"
 else
     # Just run the command as root.
     exec "$@"
