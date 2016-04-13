b905bd437ecd135b711b21f8d482407df4b2df7b
Erik De Rijcke <derijcke.erik@gmail.com>
2016-04-13 12:36:33 +0200

    add a fake toolchain file for native compilation

---
+++ linux/x86_64/Toolchain.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+
+set(CMAKE_C_COMPILER /usr/bin/gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/g++)
+
+set(CMAKE_FIND_ROOT_PATH /)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(JAVA_AWT_LIBRARY /usr/local/jvm/openjdk-1.8.0-internal/jre/lib/arm/)
+set(JAVA_JVM_LIBRARY /usr/local/jvm/openjdk-1.8.0-internal/jre/lib/arm/)
+set(JAVA_INCLUDE_PATH /usr/local/jvm/openjdk-1.8.0-internal/include/)
+set(JAVA_INCLUDE_PATH2 /usr/local/jvm/openjdk-1.8.0-internal/include/linux/)
+set(JAVA_AWT_INCLUDE_PATH /usr/local/jvm/openjdk-1.8.0-internal/include/)
