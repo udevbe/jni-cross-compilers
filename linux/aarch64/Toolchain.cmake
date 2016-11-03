4eb40ddd717587d32988e9b1a2628338684c96c5
Erik De Rijcke <derijcke.erik@gmail.com>
2016-11-03 22:05:25 +0100

    fix cmake toolchain files

--- linux/aarch64/Toolchain.cmake
+++ linux/aarch64/Toolchain.cmake
@@ -3,6 +3,7 @@ set(CMAKE_SYSTEM_VERSION 1)
 
 set(cross_triple aarch64-linux-gnu)
 set(cross_root /usr/toolchain)
+set(jvm_arch aarch64)
 
 set(CMAKE_C_COMPILER ${cross_root}/bin/${cross_triple}-gcc)
 set(CMAKE_CXX_COMPILER ${cross_root}/bin/${cross_triple}-g++)
@@ -14,8 +15,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-#set(JAVA_AWT_LIBRARY ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/jre/lib/aarch64/libawt.so)
-#set(JAVA_JVM_LIBRARY ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/jre/lib/aarch64/server/libjvm.so)
-#set(JAVA_INCLUDE_PATH ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/include/)
-#set(JAVA_INCLUDE_PATH2 ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/include/linux/)
-#set(JAVA_AWT_INCLUDE_PATH ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/include/)
+set(JAVA_AWT_LIBRARY ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/jre/lib/${jvm_arch}/libjawt.so)
+set(JAVA_JVM_LIBRARY ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/jre/lib/${jvm_arch}/server/libjvm.so)
+set(JAVA_INCLUDE_PATH ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/include/)
+set(JAVA_INCLUDE_PATH2 ${CMAKE_PREFIX_PATH}/lib/jvm/openjdk-1.8.0-internal/include/linux/)
+
--- linux/x86_64/Toolchain.cmake
+++ linux/x86_64/Toolchain.cmake
@@ -9,7 +9,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(JAVA_AWT_LIBRARY /usr/local/jvm/openjdk-1.8.0-internal/jre/lib/amd64/libawt.so)
+set(JAVA_AWT_LIBRARY /usr/local/jvm/openjdk-1.8.0-internal/jre/lib/amd64/libjawt.so)
 set(JAVA_JVM_LIBRARY /usr/local/jvm/openjdk-1.8.0-internal/jre/lib/amd64/server/libjvm.so)
 set(JAVA_INCLUDE_PATH /usr/local/jvm/openjdk-1.8.0-internal/include/)
 set(JAVA_INCLUDE_PATH2 /usr/local/jvm/openjdk-1.8.0-internal/include/linux/)
